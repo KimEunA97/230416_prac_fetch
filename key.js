2a1a10dbdfd22aa45b12515c577228c1ebadc0dd
KimEunA97 <kimeuna9712@gmail.com>
2023-04-16 23:26:18 +0900

    키 숨기기

--- key.js
+++ key.js
@@ -1,8 +1,8 @@
-const APIKey = `IGgBVJ%2BLESbzNUr3Zgld1TwbaTgjXjyTynsCnEDUeuwNeQbN7wrVenFaMf%2Bu%2FfDDDE0G4voIAFzA%2Fw9s37mTmw%3D%3D`
+const APIKey = "asd"
 
 async function getData() {
 
-  const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=IGgBVJ%2BLESbzNUr3Zgld1TwbaTgjXjyTynsCnEDUeuwNeQbN7wrVenFaMf%2Bu%2FfDDDE0G4voIAFzA%2Fw9s37mTmw%3D%3D&pageNo=1&numOfRows=1000&dataType=JSON&base_date=20230416&base_time=0600&nx=55&ny=127`
+  const url = "asd"
   const res = await fetch(url);
   const data = await res.json();
 
